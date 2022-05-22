f10d609e3e52ad19812d8e3d07b7a48d041a4ed8
vladshatilov <shatilovvlad@mail.ru>
2022-05-22 14:50:24 +0300

    fix: workflow fix

--- goals/urls.py
+++ goals/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from goals.views import GoalCategoryCreateView, GoalCategoryListView, GoalCategoryView
+from goals.views import GoalCategoryCreateView, GoalCategoryListView, GoalCategoryView, GoalCreateView, GoalListView, \
+    GoalView
 
 urlpatterns = [
     path('goal_category/create', GoalCategoryCreateView.as_view()),
     path('goal_category/list', GoalCategoryListView.as_view()),
     path("goal_category/<int:pk>", GoalCategoryView.as_view()),
+    path('goal/create', GoalCreateView.as_view()),
+    path('goal/list', GoalListView.as_view()),
+    path("goal/<int:pk>", GoalView.as_view()),
 ]
--- goals/views.py
+++ goals/views.py
@@ -42,3 +42,41 @@ class GoalCategoryView(RetrieveUpdateDestroyAPIView):
         instance.is_deleted = True
         instance.save()
         return instance
+
+
+    # Goals part
+class GoalCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCreateSerializer
+
+class GoalListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = ["title", "created"]
+    ordering = ["title"]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(
+            user=self.request.user, is_deleted=False
+        )
+
+class GoalView(RetrieveUpdateDestroyAPIView):
+    model = GoalCategory
+    serializer_class = GoalCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
+        return instance
